e83177bd9bde5860e677834092e4fdfb6cc53310
atozprasd <atozcareer@gmail.com>
2018-05-01 10:33:24 -0700

    removing config files

--- installnpm.sh
+++ installnpm.sh
@@ -3,7 +3,5 @@ sdo yum install -y wget
 wget http://nodejs.org/dist/v0.10.30/node-v0.10.30.tar.gz
 tar xzvf node-v* && cd node-v*
 sudo yum -y install gcc gcc-c++
-./configure
-make
 sudo make install
 
